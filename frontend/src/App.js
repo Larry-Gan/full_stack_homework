787476251e68d906162f79238984b39dac0799e2
Larry-Gan <lbgan2000@berkeley.edu>
2024-01-22 21:18:08 -0800

    cleaned up code a bit using eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,48 @@
-import React, { useState } from 'react';
-import './App.css';
-import JSONTreeView from './JSONTreeView';
-import axios from 'axios';
+import React, { useState } from 'react'
+import './App.css'
+import JSONTreeView from './JSONTreeView'
+import axios from 'axios'
 
-function App() {
-  const [fileContent, setFileContent] = useState('');
-  const [selectedFile, setSelectedFile] = useState(null);
+function App () {
+  const [fileContent, setFileContent] = useState('')
+  const [selectedFile, setSelectedFile] = useState(null)
 
   const handlePreviewClick = async () => {
-    console.log(selectedFile);
+    console.log(selectedFile)
     try {
-      const response = await axios.get(`http://localhost:5000/file_preview?location=${encodeURIComponent(selectedFile.location)}`);
-      setFileContent(response.data.content);
+      const response = await axios.get(`http://localhost:5000/file_preview?location=${encodeURIComponent(selectedFile.location)}`)
+      setFileContent(response.data.content)
     } catch (error) {
-      console.error('Error fetching file content: ', error);
-      setFileContent('');
+      console.error('Error fetching file content: ', error)
+      setFileContent('')
+    }
   }
-  };
 
   const handleDownloadClick = () => {
     if (!selectedFile) {
-      alert('No file selected for download.');
-      return;
+      alert('No file selected for download.')
+      return
     }
-    
-    const downloadUrl = `http://localhost:5000/file_download?location=${encodeURIComponent(selectedFile.location)}`;
-    const link = document.createElement('a');
-    link.href = downloadUrl;
-    link.setAttribute('download', selectedFile.name); 
-    document.body.appendChild(link);
-    link.click();
-    link.parentNode.removeChild(link);
-  };
-  
+
+    const downloadUrl = `http://localhost:5000/file_download?location=${encodeURIComponent(selectedFile.location)}`
+    const link = document.createElement('a')
+    link.href = downloadUrl
+    link.setAttribute('download', selectedFile.name)
+    document.body.appendChild(link)
+    link.click()
+    link.parentNode.removeChild(link)
+  }
 
   // Don't preview if the files are 3D
-  const previewAvailable = selectedFile && !(selectedFile.name.endsWith(".stl") || selectedFile.name.endsWith(".step") || selectedFile.name.endsWith(".ply"));
+  const previewAvailable = selectedFile && !(selectedFile.name.endsWith('.stl') || selectedFile.name.endsWith('.step') || selectedFile.name.endsWith('.ply'))
 
   return (
     <div className="App">
       <div className="app-content">
-        <JSONTreeView setSelectedFile={setSelectedFile} setFileContent={setFileContent} selectedFile={selectedFile}/>
+        <div>
+          <JSONTreeView setSelectedFile={setSelectedFile} setFileContent={setFileContent} selectedFile={selectedFile}/>
+        </div>
+
         <div className="content-display">
           <div className="file-actions">
             {selectedFile && (
@@ -54,11 +56,11 @@ function App() {
               </>
             )}
           </div>
-          <pre>{selectedFile && !previewAvailable ? "3D files are not available for preview" : fileContent}</pre>
+          <pre>{selectedFile && !previewAvailable ? '3D files are not available for preview' : fileContent}</pre>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/JSONTreeView.js
+++ frontend/src/JSONTreeView.js
@@ -1,20 +1,21 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { TreeView } from '@mui/x-tree-view/TreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import axios from 'axios';
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box'
+import { TreeView } from '@mui/x-tree-view/TreeView'
+import { TreeItem } from '@mui/x-tree-view/TreeItem'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import axios from 'axios'
 
-function createTreeItem(node, nodeId, handleFileClick) {
-    const isLeaf = (key) => ['Form', 'CAD', 'Scan'].includes(key);
-  
-    return (
+function createTreeItem (node, nodeId, handleFileClick) {
+  const isLeaf = (key) => ['Form', 'CAD', 'Scan'].includes(key)
+
+  return (
       <TreeItem key={nodeId} nodeId={nodeId} label={nodeId.split('/').pop()}>
         {Object.keys(node).map((childId) => {
-          const newId = `${nodeId}/${childId}`;
-          const childNode = node[childId];
-  
+          const newId = `${nodeId}/${childId}`
+          const childNode = node[childId]
+
           // Handles the leaves at the end of the json
           if (isLeaf(childId) && Array.isArray(childNode)) {
             return (
@@ -28,64 +29,64 @@ function createTreeItem(node, nodeId, handleFileClick) {
                   />
                 ))}
               </TreeItem>
-            );
+            )
           }
-  
+
           // Recursively create tree
           if (childNode instanceof Object && !(childNode instanceof Array)) {
-            return createTreeItem(childNode, newId, handleFileClick);
+            return createTreeItem(childNode, newId, handleFileClick)
           }
-  
+
           // Return branch
-          return <TreeItem key={newId} nodeId={newId} label={childId} />;
+          return <TreeItem key={newId} nodeId={newId} label={childId} />
         })}
       </TreeItem>
-    );
-  }
-  
-  export default function JSONTreeView({ setSelectedFile, setFileContent, selectedFile }) {
-    const [jsonData, setJsonData] = React.useState(null);
-  
-    // Get file data from backend
-    React.useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('http://localhost:5000/directory');
-          setJsonData(response.data);
-        } catch (error) {
-          console.error('Error fetching data: ', error);
-        }
-      };
-  
-      fetchData();
-    }, []);
+  )
+}
 
-    // Handle clicking on the leaves
-    const handleFileClick = (node) => {
-        if (!selectedFile || node.uuid !== selectedFile.uuid) {
-            setSelectedFile(node);
-            setFileContent('');
-        }
-    };
+function JSONTreeView ({ setSelectedFile, setFileContent, selectedFile }) {
+  const [jsonData, setJsonData] = React.useState(null)
 
-    const renderTree = (nodes) => (
-        Object.keys(nodes).map((nodeId) => {
-          const rootId = `/${nodeId}`;
-          if (nodes[nodeId] instanceof Object && !(nodes[nodeId] instanceof Array)) {
-            return createTreeItem(nodes[nodeId], rootId, handleFileClick);
-          } else {
-            // Just in case
-            return <TreeItem key={rootId} nodeId={rootId} label={nodeId} />;
-          }
-        })
-    );
-  
-    // If there's no data, display this instead of the tree
-    if (!jsonData) {
-      return <div>Loading...</div>;
+  // Get file data from backend
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/directory')
+        setJsonData(response.data)
+      } catch (error) {
+        console.error('Error fetching data: ', error)
+      }
     }
-  
-    return (
+
+    fetchData()
+  }, [])
+
+  // Handle clicking on the leaves
+  const handleFileClick = (node) => {
+    if (!selectedFile || node.uuid !== selectedFile.uuid) {
+      setSelectedFile(node)
+      setFileContent('')
+    }
+  }
+
+  const renderTree = (nodes) => (
+    Object.keys(nodes).map((nodeId) => {
+      const rootId = `/${nodeId}`
+      if (nodes[nodeId] instanceof Object && !(nodes[nodeId] instanceof Array)) {
+        return createTreeItem(nodes[nodeId], rootId, handleFileClick)
+      } else {
+        // Just in case
+        return <TreeItem key={rootId} nodeId={rootId} label={nodeId} />
+      }
+    })
+  )
+
+  // If there's no data, display this instead of the tree
+  if (!jsonData) {
+    return <div>Loading...</div>
+  }
+
+  return (
         <div className="JSONTreeView">
             <h2>Customers</h2>
             <Box sx={{ height: '100vh', flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}>
@@ -99,5 +100,18 @@ function createTreeItem(node, nodeId, handleFileClick) {
                 </TreeView>
             </Box>
         </div>
-    );
-  }
+  )
+}
+
+// Define the prop types
+JSONTreeView.propTypes = {
+  setSelectedFile: PropTypes.func.isRequired,
+  setFileContent: PropTypes.func.isRequired,
+  selectedFile: PropTypes.shape({
+    uuid: PropTypes.string,
+    name: PropTypes.string,
+    location: PropTypes.string
+  })
+}
+
+export default JSONTreeView
